4cd2a7e7b1cb2da7cc3737ef510fc7513a918b2b
Florangl <floranainggolan100905@gmail.com>
2024-06-07 10:05:24 +0700

    Upload File

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center">
+  <img src="{{ asset('images/logoku.png') }}" alt="" class="h-16 w-16">
+  <h1 class="text-3xl font-bold text-green-500">Lapangan Badminton </h1>
+</div>
